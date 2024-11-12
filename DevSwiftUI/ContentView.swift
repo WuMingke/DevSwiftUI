68dcc906f16c4090936cec614fa110d76c272027
wumingke <766190553@qq.com>
2024-11-12 23:46:55 +0800

    init

---
+++ DevSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DevSwiftUI
+//
+//  Created by 吴清冽 on 2024/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "heart.fill")
+                .imageScale(.large)
+                .foregroundColor(.red)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DevSwiftUI/DevSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevSwiftUIApp.swift
+//  DevSwiftUI
+//
+//  Created by 吴清冽 on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct DevSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
